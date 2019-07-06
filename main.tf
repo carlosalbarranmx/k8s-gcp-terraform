0667fbe09b66518845da25d401477fdf18e0f4fd
Carlos Albarrán <carlos.albarran.mx@gmail.com>
2019-07-06 18:52:05 -0500

    Update main.tf: US region

--- main.tf
+++ main.tf
@@ -38,5 +38,5 @@ resource "google_container_cluster" "kubernetes" {
 # Fetches the project name, and provides the appropriate URLs to use for container registry.
 data "google_container_registry_repository" "kubernetes" {
   # The GCR region to use.
-  region = "eu"
+  region = "us"
 }
